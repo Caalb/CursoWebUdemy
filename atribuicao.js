73f7674d0dc9ea3b3014d21bdc6f9619ae5b24a0
Carlos Alberto <caalb.craft@gmail.com>
2022-01-03 17:37:48 -0300

    Operadores de atribuição

---
+++ atribuicao.js
@@ -0,0 +1,20 @@
+ const a = 7;
+ let b = 3;
+
+ b += a // Está atribuindo e somando b com a (b = b + a)
+ console.log(b);
+
+ b -= 4 // Está atribuindo e subtraindo.
+ console.log(b);
+
+ b *= 2 //  Está atribuindo e multiplicando.
+ console.log(b);
+
+ b /= 2 // Está atribuindo e dividindo.
+ console.log(b);
+
+ b %= 2 // Está atribuindo e fazendo o módulo.
+ console.log(b);
+
+
+ 
