f69026556cf52d4f04c9a1371e2523762bfafee8
Carlos Alberto <caalb.craft@gmail.com>
2022-01-03 21:58:09 -0300

    operadores aritmeticos

---
+++ aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const soma = a + b + c + d;
+const subtracao = d - b;
+const multiplicacao = a * b;
+const divisao = d / a;
+const modulo = a % 2 // Este módulo é para ver se tem resto da divisão no número (muito usando para descobrir se um número é par)
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
