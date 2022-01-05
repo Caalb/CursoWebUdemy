7b68906de9e57af58d65fb20bdef9cf66d8f0efd
Carlos Alberto <caalb.craft@gmail.com>
2022-01-05 15:46:30 -0300

    if/else

---
+++ 2-EstruturasDeControleJS/if1.js
@@ -0,0 +1,16 @@
+function soBoaNoticia(nota) {
+    if(nota >= 7) {
+        console.log('Aprovado com ',nota)
+    }
+}
+
+soBoaNoticia(8);
+soBoaNoticia(6.1);
+
+function seForVerdadeEuFalo(valor) {
+    if(valor) {
+        console.log('É verdade...', valor)
+    }
+}
+
+seForVerdadeEuFalo();
---
+++ 2-EstruturasDeControleJS/if2.js
@@ -0,0 +1,17 @@
+function teste1(num) {
+    if(num > 7) 
+        console.log(num)
+
+    console.log('Final')    
+}
+
+teste1(6);
+teste1(8);
+
+ function teste2(num) {
+     if(num > 7);      // Não colocar este ponto e virgula por conta que ele separa o if do console.log
+        console.log(num)
+}
+
+teste2(6);
+teste2(8)
