46521ed73e522d9970f9666ef77dd73ac5fd93eb
Carlos Alberto <caalb.craft@gmail.com>
2022-01-27 16:49:15 -0300

    Introdução à OO

---
+++ 4-ObjetoJS/oo.js
@@ -0,0 +1,19 @@
+// CÓDIGO NÃO EXECUTAVEL 
+
+// Procedural
+
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = { 
+    valor1,
+    valor2,
+    valor3, 
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco  passou a ser objeto e não as funções
+
+ 
